5d56e636a7c4eb7d23d6ccefe03d422b2c83c812
Chris Cantu <pinion31@gmail.com>
2017-07-03 21:30:09 -0500

    code complete

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var express = require('express');
+
+var multer = require('multer');
+var upload = multer({dest:'uploads/'});
 var app = express();
+app.use(express.static('static'));
+
+app.post('/', upload.single('file'), function(req,res, next) {
+    res.end("File size is " + JSON.stringify(req.file.size) + " bytes.");
+});
 
 
-
+app.listen(3000);
