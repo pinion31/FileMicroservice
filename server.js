c19a93b95cfa93aebc9ac7372c5c66dd10084beb
Chris Cantu <pinion31@gmail.com>
2017-07-03 21:56:32 -0500

    fixed filepath

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express');
 var multer = require('multer');
 var upload = multer({dest:'uploads/'});
 var app = express();
-app.use(express.static('static'));
+
+
+app.use(express.static(__dirname + '/static'));
 
 app.post('/', upload.single('file'), function(req,res, next) {
     res.end("File size is " + JSON.stringify(req.file.size) + " bytes.");
